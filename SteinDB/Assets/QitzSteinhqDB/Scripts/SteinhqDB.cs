339e87e48b53ae538ab1dadaa41097b93fac3236
USER <user@USERnoMacBook-Pro.local>
2019-08-24 14:43:22 +0900

    データ取得周りの実装完了

---
+++ SteinDB/Assets/QitzSteinhqDB/Scripts/SteinhqDB.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Linq;
+using UnityEngine.Networking;
+
+namespace Qitz.SteinhqDB
+{
+    public interface ISteinhqDB
+    {
+        IEnumerator GetData<T>(Action<List<T>> callBack);
+    }
+
+    public static class SteinhqDBFactory
+    {
+        class SteinhqDB : ISteinhqDB
+        {
+            string steinhqApiUrl = "";
+            string targetSheetName = "";
+            string targetAPIUrl => $"{steinhqApiUrl}/{targetSheetName}";
+
+            public SteinhqDB(string steinhqApiUrl, string targetSheetName)
+            {
+                this.steinhqApiUrl = steinhqApiUrl;
+                this.targetSheetName = targetSheetName;
+            }
+
+            public IEnumerator GetData<T>(Action<List<T>> callBack)
+            {
+                UnityWebRequest request = UnityWebRequest.Get(targetAPIUrl);
+                yield return request.Send();
+                if (request.isNetworkError)
+                {
+                    Debug.LogError(request.error);
+                    yield break;
+                }
+                string json = request.downloadHandler.text;
+                //先頭行と最終行の文字列を削除
+                json = json.TrimStart('[');
+                json = json.TrimEnd(']');
+                //jsonの配列取得
+                string[] jsonArray = GetJsonArray(json);
+                callBack(jsonArray.Select(j => JsonUtility.FromJson<T>(j)).ToList());
+            }
+
+            string[] GetJsonArray(string json)
+            {
+                string[] del = { "}," };
+                string[] jsonArray = json.Split(del, StringSplitOptions.None);
+                for (int i = 0; i < jsonArray.Length; i++)
+                {
+                    string rightString = jsonArray[i].Substring(jsonArray[i].Length - 1, 1);
+                    if (rightString != "}")
+                    {
+                        jsonArray[i] = jsonArray[i] + "}";
+                    }
+                }
+                return jsonArray;
+            }
+
+        }
+
+        public static ISteinhqDB Create(string steinhqApiUrl, string targetSheetName)
+        {
+            return new SteinhqDB(steinhqApiUrl, targetSheetName);
+        }
+    }
+
+}
---
+++ SteinDB/Assets/QitzSteinhqDB/Tests/SteinDBTests.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using Qitz.SteinhqDB;
+using System;
+
+namespace Tests
+{
+    [Serializable]
+    public class TestClassObejct
+    {
+        [SerializeField]
+        int id;
+        public int Id { get { return id; } }
+
+        [SerializeField]
+        string name;
+        public string Name { get { return name; } }
+
+        [SerializeField]
+        string discription;
+        public string Discription { get { return discription; } }
+    }
+
+    public class SteinDBTests
+    {
+
+        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+        // `yield return null;` to skip a frame.
+        [UnityTest]
+        public IEnumerator SteinhqDBからデータを取得するテスト()
+        {
+            var db = SteinhqDBFactory.Create("https://api.steinhq.com/v1/storages/5d6093ecbb4eaf04c5eaa2b5", "test_data");
+            yield return  db.GetData<TestClassObejct>((data)=> {
+                foreach (var d in data)
+                {
+                    Debug.Log(d.Name);
+                }
+            });
+
+            yield return null;
+        }
+    }
+}
